f20c80495f0a9ccf60ced0808baebfc1a7793261
Saisharvesh07 <72559801+Saisharvesh07@users.noreply.github.com>
2021-02-01 19:46:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+menu_list_array = ["Chicken Supreme Pizza",
+                   "Indi Paneer Pizza",
+                   "Deluxe Veggie Pizza",
+                   "Chicken Tandoori Pizza",
+                   "Veg Extravaganza Pizza"];
+
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+    function add_item(){
+        var htmldata;
+        var item=document.getElementById("add item").nodeValue;
+        menu_list_array.push(item);
+        menu_list_array.sort();
+        htmldata="<section class='cards'>"
+        for(var i=0;1<menu_list_array.length;i++){
+
+
+            htmldata=htmldata= "<div class='card'"> +'<img src="images/plzzalimg.png"/>' +menu_list_array[1] + '</div>'
+
+        }
+
+        htmldata=htmldata+"</section>"
+        document.getElementById("display_addedmenu").innerHTML = htmldata
+    }
+
+
+
+
+
+
+
+
+
+
+
